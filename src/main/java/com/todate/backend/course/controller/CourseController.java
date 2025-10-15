d1ab26ad30c3d5c769cf31732f11c7b296f11160
0Cracker <gogoseo11084@gmail.com>
2025-10-15 22:26:56 +0900

    [FIX] coursecontroller 변경 -request body에서 username 제거 -url parameter username 제거 - 모두 @authen 에서 받아오는 방식으로 변경

--- src/main/java/com/todate/backend/course/controller/CourseController.java
+++ src/main/java/com/todate/backend/course/controller/CourseController.java
@@ -1,10 +1,7 @@
 package com.todate.backend.course.controller;
 
 import java.net.URI;
-import java.util.List;
-import java.util.Objects;
 
-import org.antlr.v4.runtime.misc.ObjectEqualityComparator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,11 +9,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.todate.backend.course.domain.Course;
 import com.todate.backend.course.dto.response.CourseResponse;
 import com.todate.backend.course.service.CourseService;
 
@@ -32,12 +27,10 @@ public class CourseController {
     //데이트 코스 생성
     @PostMapping
     public ResponseEntity<Void> createCourse(
-            @RequestBody String username,
             @AuthenticationPrincipal UserDetails userDetails){
+
         String tokenUsername = userDetails.getUsername();
-        if(!Objects.equals(username, tokenUsername)){
-            throw new IllegalStateException("요청된 사용자와 인증된 사용자가 일치하지 않습니다.")
-        }
+
         Long courseId = courseService.CreateCourse(tokenUsername);
         URI location = URI.create("/api/v1/course/" + courseId);
         return ResponseEntity.created(location).build();
@@ -46,13 +39,9 @@ public class CourseController {
     //데이트 코스 조회
     @GetMapping("/{username}")
     public ResponseEntity<CourseResponse> getCourses(
-            @PathVariable("username") String pathUsername,
             @AuthenticationPrincipal UserDetails userDetails){
         
         String tokenUsername = userDetails.getUsername();
-        if(!Objects.equals(pathUsername, tokenUsername)){
-            throw new IllegalStateException("요청된 사용자와 인증된 사용자가 일치하지 않습니다.");
-        }
 
         CourseResponse response = courseService.findAllCourses(tokenUsername);
         return ResponseEntity.ok(response);
